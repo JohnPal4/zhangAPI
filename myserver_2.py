c42c1f65af7f6793897c0c65034724bc69daf2ae
JohnPal4 <palchizacajohn@gmail.com>
2026-04-16 23:44:37 -0400

    Update myserver_2.py

--- myserver_2.py
+++ myserver_2.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from supabase import create_client, Client
+from fastapi.responses import HTMLResponse
 import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+INDEX_PATH = os.path.join(BASE_DIR, "index.html")
 
 app = FastAPI()
 
@@ -9,7 +13,12 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
+def serve_home():
+    with open(INDEX_PATH, "r", encoding="utf-8") as f:
+        return f.read()
+
+@app.get("/api")
 async def root():
     response = supabase.table("president").select("*", count="exact").execute()
     return {
